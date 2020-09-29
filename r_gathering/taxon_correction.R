d0cdf1734c67953e68b05fb626e133c7059d5fea
Paul Efren <paulefrens@gmail.com>
2020-09-29 18:55:16 -0500

    Add files via upload

---
+++ r_gathering/taxon_correction.R
@@ -0,0 +1,61 @@
+
+GenusDictionary2020 <- read.table(header = TRUE, 
+                                  stringsAsFactors = FALSE, 
+                                  text = "wrong right
+                                  Anatherastipa Anatherostipa
+                                  Antenneria Antennaria
+                                  Belonathus Belonanthus
+                                  Caramagrostis Calamagrostis
+                                  COrtaderia Cortaderia
+                                  Gautheria Gaultheria
+                                  Helenia Halenia
+                                  Jasmesonia Jamesonia
+                                  PAspalum Paspalum
+                                  Oreomirhys Oreomyrrhis
+                                  Oreomyrhys Oreomyrrhis
+                                  Oriethales Oreithales
+                                  Oriotrophium Oritrophium
+                                  Oritrophium Oritrophium
+                                  rhynchospora Rhynchospora
+                                  Rhyncospora Rhynchospora
+                                  Rynchospora Rhynchospora
+                                  Tricophorum Trichophorum"
+)
+
+
+
+SpeciesDictionary2020 <- read.table(header = TRUE, 
+                                    stringsAsFactors = FALSE, 
+                                    text = "wrong right
+                                    tricoides trichodes
+                                    'hans meyeni' hans-meyeri
+                                    hans-meyen hans-meyeri
+                                    beauty 'cf. macrophylla'
+                                    tri trichophylla
+                                    nigida rigida
+                                    boliniensis boliviensis
+                                    bolivuensis boliviensis
+                                    pinchinchensis pichinchensis
+                                    pinchenchensis pichinchensis
+                                    pigmea pygmaea
+                                    pigmeo pygmaea
+                                    pygmea pygmaea
+                                    cheilantoides cheilanthoides
+                                    chelantoides cheilanthoides
+                                    sharp sp4
+                                    ennestii ernestii
+                                    tanaxacoides taraxacoides
+                                    aistonia  alstonii
+                                    alstonia alstonii
+                                    arbiculata orbiculata
+                                    racem racemosa
+                                    racemoss racemosa
+                                    vacenosa racemosa
+                                    claratum clavatum
+                                    hieracoides hieracioides
+                                    ore oreocharis
+                                    pugens pungens
+                                    postrata prostrata
+                                    spicata spicigera
+                                    andinium andinum"
+)
