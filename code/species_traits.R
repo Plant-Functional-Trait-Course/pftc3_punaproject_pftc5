384c3c5da8ce38ad5d5eb8f4a5ddd12f2b59b4af
TanyaS08 <tanya.strydom@icloud.com>
2021-03-01 15:52:26 +0200

    family, Fgroup NAs Puna Proj
    There were a few samples that threw NAs for fome of the info related to taxon. I think they werent in the species dictionary...

--- code/species_traits.R
+++ code/species_traits.R
@@ -59,6 +59,9 @@ trait_2019 %>%
   count(id) %>%
   distinct(n)
 
+trait_2019 %>%
+  filter(is.na(taxon))
+
 ###############################################################
 #' Merging data sets: add species names cleaned
 #'
@@ -130,13 +133,29 @@ trait_puna <- trait_2019 %>%
          ldmc = dry_mass_g / wet_mass_g) %>%
   # Reordering columns for matching with the other dataset
   select(country, project, id, year, month, date, site, treatment, plot_id,
-         functional_group, family, name_2020, genus, species,
+         functional_group, family, taxon, genus, species,
          individual_nr, plant_height_cm, nr_leaves, bulk, wet_mass_total_g,
          leaf_thickness_1_mm, leaf_thickness_2_mm, leaf_thickness_3_mm,
          dry_mass_total_g, number_leaves_scan, leaf_area_total_cm2,
          wet_mass_g, dry_mass_g, leaf_area_cm2, sla_cm2_g, ldmc,
-         leaf_thickness_ave_mm, area_flag, dry_flag, wet_flag  )
-
+         leaf_thickness_ave_mm, area_flag, dry_flag, wet_flag  ) %>%
+  #Some of these species were not in the dictionary thus now have NA's
+  mutate(functional_group = case_when(taxon == "Carex pichinchensis" ~ "Gramminoid",
+                                      taxon == "Rinchospora machrochaeta fina" ~ "Gramminoid",
+                                      taxon == "Rinchospora machrochaeta gruesa" ~ "Gramminoid",
+                                      TRUE ~ functional_group),
+         family = case_when(taxon == "Carex pichinchensis" ~ "Cyperaceae",
+                            taxon == "Rinchospora machrochaeta fina" ~ "Cyperaceae",
+                            taxon == "Rinchospora machrochaeta gruesa" ~ "Cyperaceae",
+                            TRUE ~ family),
+         species = case_when(taxon == "Carex pichinchensis" ~ "pichinchensis",
+                             taxon == "Rinchospora machrochaeta fina" ~ "machrochaeta fina",
+                             taxon == "Rinchospora machrochaeta gruesa" ~ "machrochaeta gruesa",
+                             TRUE ~ species),
+         genus = case_when(taxon == "Carex pichinchensis" ~ "Carex",
+                           taxon == "Rinchospora machrochaeta fina" ~ "Rinchospora",
+                           taxon == "Rinchospora machrochaeta gruesa" ~ "Rinchospora",
+                           TRUE ~ genus))
 
 ########################################################################################
 # PFTC5 2020 PERU
@@ -477,7 +496,7 @@ trait_pftc5 <- trait_pftc5 %>%
          dry_mass_total_g = dry_mass_g,
          # correction - this is now the number of leafs
          nr_leaves = bulk_nr_leaves,
-         #this is leaf neumber per an individual
+         #this is leaf number per an individual
          leaf_nr = leaf_nr,
          dry_flag = drymassflag,
          wet_flag = wetflag,
