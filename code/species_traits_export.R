9f33fac06ab61579eccdc3351f4060e9a095d74a
audhalbritter <me@audhalbritter.com>
2021-03-09 13:18:35 +0100

    create new folder called clean_data

--- code/species_traits_export.R
+++ code/species_traits_export.R
@@ -1,6 +1,8 @@
 # load libraries
 source("code/load_libraries.R")
 
+library("janitor")
+
 ## Read scripts for the three traits datasets ----
 source(here::here("code/species_traits_pftc3.R")) #PFTC3 import and clean
 source(here::here("code/species_traits_punaproject.R")) #Puna Project import and clean
@@ -14,31 +16,30 @@ trait_data_peru <- bind_rows(trait_pftc3,
 
 ## Export data ----
 
+# make new folder
+dir.create("clean_data")
+
 #PFTC3
 trait_pftc3 %>%
-  distinct(project)
+  distinct(course)
 
 trait_pftc3 %>%
   write_csv("clean_data/PFTC3_Peru_2018_LeafTraits_clean.csv")
 
 #PUNA PROJECT
 trait_puna %>%
-  distinct(project)
-
-trait_puna %>%
-  filter(id == "AZJ4672") %>%
-  pull(dry_mass_g)
+  distinct(course)
 
 trait_puna %>%
   write_csv("clean_data/PunaProject_Peru_2019_LeafTraits_clean.csv")
 
 #PFTC5
 trait_pftc5 %>%
-  distinct(project)
+  distinct(course)
 
 trait_pftc5 %>%
-  filter(treatment != "OFF-PLOT")
-write_csv("clean_data/PFTC5_Peru_2020_LeafTraits_clean.csv")
+  filter(treatment != "OFF-PLOT") %>%
+  write_csv("clean_data/PFTC5_Peru_2020_LeafTraits_clean.csv")
 
 # PFTC3 - Puna Project - PFTC5
 trait_data_peru %>%
@@ -47,7 +48,7 @@ trait_data_peru %>%
 #Sean...
 
 trait_pftc5 %>%
-  filter(treatment == "OFF-PLOT")
-write_csv("clean_data/PFTC5_Peru_2020_Seans_LeafTraits_clean.csv")
+  filter(treatment == "OFF-PLOT") %>%
+  write_csv("clean_data/PFTC5_Peru_2020_Seans_LeafTraits_clean.csv")
 
 # End of Script ----
