7ed11c1363add61bde13df2f82e23459778a18aa
audhalbritter <me@audhalbritter.com>
2021-03-15 11:20:20 +0100

    fix merging of traits

--- code/species_traits_export.R
+++ code/species_traits_export.R
@@ -1,6 +1,5 @@
 # load libraries
 source("code/load_libraries.R")
-
 library("janitor")
 
 ## Read scripts for the three traits datasets ----
@@ -13,43 +12,47 @@ source(here::here("code/species_traits_pftc5.R")) #PFTC5 import and clean
 trait_data_peru <- bind_rows(trait_pftc3,
                              trait_puna,
                              trait_pftc5) %>%
-  pivot_longer(cols = plant_height_cm:leaf_thickness_mm, names_to = "trait", values_to = "value")
+
+  # make data long
+  pivot_longer(cols = plant_height_cm:leaf_thickness_mm, names_to = "trait", values_to = "value") %>%
+  filter(!is.na(value)) %>%
+
+  mutate(flag = case_when(wet_flag == "Outlier_very_large_leaf" ~ "outlier",
+                          area_flag == "Outlier_very_large_leaf" ~ "outlier",
+                          area_flag == "Leaf too white_Area missing" ~ "scanning_too_white",
+                          area_flag == "Area estimated" ~ "area_estimated")) %>%
+  select(-area_flag, -dry_flag, -wet_flag)
+
+
+
+
+#trait_data_peru %>% distinct(course, dry_flag)
 
 ## Export data ----
 
 # make new folder
 dir.create("clean_data")
 
-#PFTC3
-trait_pftc3 %>%
-  distinct(course)
-
-trait_pftc3 %>%
-  write_csv("clean_data/PFTC3_Peru_2018_LeafTraits_clean.csv")
-
-#PUNA PROJECT
-trait_puna %>%
-  distinct(course)
-
-trait_puna %>%
-  write_csv("clean_data/PunaProject_Peru_2019_LeafTraits_clean.csv")
-
-#PFTC5
-trait_pftc5 %>%
-  distinct(course)
-
-trait_pftc5 %>%
-  filter(treatment != "OFF-PLOT") %>%
-  write_csv("clean_data/PFTC5_Peru_2020_LeafTraits_clean.csv")
-
-# PFTC3 - Puna Project - PFTC5
 trait_data_peru %>%
-  distinct(project)
+  write_csv("clean_data/PFTC3-Puna-PFTC5_Peru_2018-2020_LeafTraits_clean.csv")
 
-#Sean...
-
-trait_pftc5 %>%
-  filter(treatment == "OFF-PLOT") %>%
-  write_csv("clean_data/PFTC5_Peru_2020_Seans_LeafTraits_clean.csv")
 
 # End of Script ----
+
+# PROBLEMS!!!
+# pftc3: BTW3395 d > w, ldmc > 1
+# pftc3: DSP7480 d > w, ldmc > 1
+
+#
+# trait_data_peru %>%
+#   filter(trait == "ldmc" & value > 0.99) %>% View()
+# #filter(wet_mass_g == 0 | dry_mass_g == 0) %>%
+# # arrange(site, treatment, plot_id, species) %>% View()
+# #filter(is.na(leaf_thickness_mm)) %>% View()
+#
+# trait_data_peru %>%
+#   mutate(value = if_else(trait %in% c("wet_mass_g", "dry_mass_g", "leaf_area_cm2"), log(value), value)) %>%
+#   #filter(trait == "leaf_area_cm2") %>%
+#   ggplot(aes(x = log(value))) +
+#   geom_density() +
+#   facet_wrap(~ trait, scales = "free")
