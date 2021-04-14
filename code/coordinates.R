ce244aa01fbe35072c567b7da207d4b1ebceba39
audhalbritter <me@audhalbritter.com>
2021-04-14 16:17:56 +0200

    add import code for coordinates

---
+++ code/coordinates.R
@@ -0,0 +1,9 @@
+## read in coordinate
+
+# Load libraries ----------------------------------------------------------
+source("code/load_libraries.R")
+library("janitor")
+
+coordinates <- read_excel("data/PU.10_PFTC3.10_2020_Peru_Coordinates.xlsx") %>%
+  clean_names() %>%
+  mutate(plot_id = as.character(plot_id))
