9956b43d3084fee54e05f0dac4dcb45eb4f73d66
audhalbritter <me@audhalbritter.com>
2021-04-17 10:43:15 +0200

    remove code to remind of remaining problems. all are solved

--- code/species_traits_export.R
+++ code/species_traits_export.R
@@ -26,14 +26,6 @@ trait_data_peru <- bind_rows(trait_pftc3,
   left_join(coordinates, by = c("site", "treatment", "plot_id")) %>%
   select(-comment)
 
-# problems to fix (have checked first two. the others will be checked)
-trait_data_peru %>%
-  anti_join(coordinates, by = c("site", "treatment", "plot_id")) %>%
-  distinct(course, site, treatment, plot_id)
-
-
-#trait_data_peru %>% distinct(course, dry_flag)
-
 ## Export data ----
 
 # make new folder
@@ -44,21 +36,3 @@ trait_data_peru %>%
 
 
 # End of Script ----
-
-# PROBLEMS!!!
-# pftc3: BTW3395 d > w, ldmc > 1
-# pftc3: DSP7480 d > w, ldmc > 1
-
-#
-# trait_data_peru %>%
-#   filter(trait == "ldmc" & value > 0.99) %>% View()
-# #filter(wet_mass_g == 0 | dry_mass_g == 0) %>%
-# # arrange(site, treatment, plot_id, species) %>% View()
-# #filter(is.na(leaf_thickness_mm)) %>% View()
-#
-# trait_data_peru %>%
-#   mutate(value = if_else(trait %in% c("wet_mass_g", "dry_mass_g", "leaf_area_cm2"), log(value), value)) %>%
-#   #filter(trait == "leaf_area_cm2") %>%
-#   ggplot(aes(x = log(value))) +
-#   geom_density() +
-#   facet_wrap(~ trait, scales = "free")
