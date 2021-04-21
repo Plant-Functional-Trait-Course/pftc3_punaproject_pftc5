11ff781083b7e59b349497a21e03d83775e729ca
audhalbritter <me@audhalbritter.com>
2021-04-21 09:28:09 +0200

    update climate download code

--- code/0_download_raw_data.R
+++ code/0_download_raw_data.R
@@ -3,10 +3,12 @@
 source("code/load_libraries.R")
 
 # Download raw data
+# coordinates
 download_PFTC_data(country = "Peru",
                    datatype = "meta",
                    path = "data")
 
+# traits
 download_PFTC_data(country = "Peru",
                    datatype = "raw_traits",
                    path = "data")
@@ -24,7 +26,21 @@ zipFile <- "data/PFTC5_Peru_2020_Raw_Trait_Data.zip"
 outDir <- "data/raw_traits_2020"
 unzip(zipFile, exdir = outDir)
 
+# community data
 download_PFTC_data(country = "Peru",
                    datatype = "raw_community",
                    path = "data")
 
+# climate
+download_PFTC_data(country = "Peru",
+                   datatype = "raw_climate",
+                   path = "data/climate")
+
+# Unzip files
+zipFile <- "data/climate/tomst_2019/"
+outDir <- "data/climate/tomst_2019/"
+unzip(zipFile, exdir = outDir)
+
+zipFile <- "data/climate/tomst_2020/"
+outDir <- "data/climate/tomst_2020/"
+unzip(zipFile, exdir = outDir)
