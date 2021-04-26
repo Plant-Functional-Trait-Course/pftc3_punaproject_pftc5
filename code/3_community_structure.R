824171622c6a39bbbdf72de8943eefde40738603
audhalbritter <me@audhalbritter.com>
2021-04-26 12:13:23 +0200

    change functional group to variable class

--- code/3_community_structure.R
+++ code/3_community_structure.R
@@ -23,20 +23,20 @@ comm_structure_raw_2018 <- read_csv("data/PFTC3_rawmetaCommunity_2018_Peru.csv")
 
 comm_structure_2018 <- comm_structure_raw_2018 %>%
   select(-c(day, photo, elevation, max_height_cm:median_height_cm, cover_shrub_layer:cover_bottom_layer)) %>%
-  pivot_longer(cols = c(cover_forbs:cover_litter), names_to = c("variable" , "functional_group"), names_sep = "_", values_to = "value") %>%
+  pivot_longer(cols = c(cover_forbs:cover_litter), names_to = c("variable" , "variable_class"), names_sep = "_", values_to = "value") %>%
   # add height data
   bind_rows(comm_structure_raw_2018 %>%
               select(site, year, month, plot_id, treatment, max_height_cm:median_height_cm) %>%
               pivot_longer(cols = c(max_height_cm:median_height_cm), names_to = "variable", values_to = "value") %>%
               mutate(variable = str_remove(variable, "\\_cm"),
-                     functional_group = "vegetation")) %>%
+                     variable_class = "vegetation")) %>%
   # add sum of cover
   bind_rows(comm_structure_raw_2018 %>%
               select(site, year, month, plot_id, treatment, cover_shrub_layer:cover_bottom_layer) %>%
-              pivot_longer(cols = c(cover_shrub_layer:cover_bottom_layer), names_to = c("functional_group"), values_to = "value") %>%
-              mutate(functional_group = str_remove(functional_group, "cover\\_"),
+              pivot_longer(cols = c(cover_shrub_layer:cover_bottom_layer), names_to = c("variable_class"), values_to = "value") %>%
+              mutate(variable_class = str_remove(variable_class, "cover\\_"),
                      variable = "cover")) %>%
-  mutate(functional_group = if_else(functional_group == "open", "bare_ground", functional_group),
+  mutate(variable_class = if_else(variable_class == "open", "bare_ground", variable_class),
          course = "PFTC3") %>%
   filter(!is.na(value))
 
@@ -71,18 +71,18 @@ comm_structure_raw_2019 <- read_csv("data/PU.2_Community metadata_Dataset.csv") 
 
 comm_structure_2019 <- comm_structure_raw_2019 %>%
   select(-c(max_height_cm:bryophyte_depth)) %>%
-  pivot_longer(cols = c(cover_graminoids:cover_solid_litter), names_to = c("variable" , "functional_group"), names_sep = "_", values_to = "value") %>%
+  pivot_longer(cols = c(cover_graminoids:cover_solid_litter), names_to = c("variable" , "variable_class"), names_sep = "_", values_to = "value") %>%
   # add height data
   bind_rows(comm_structure_raw_2019 %>%
               select(site, year, month, plot_id, treatment, max_height_cm:bryophyte_depth) %>%
               pivot_longer(cols = c(max_height_cm:bryophyte_depth), names_to = "variable", values_to = "value") %>%
               mutate(variable = str_remove(variable, "\\_cm"),
-                     functional_group = case_when(variable == "bryophyte_depth" ~ "bryophyte",
+                     variable_class = case_when(variable == "bryophyte_depth" ~ "bryophyte",
                                                   TRUE ~ "vegetation"))) %>%
-  mutate(functional_group = case_when(functional_group == "open" ~ "bare_ground",
-                                      functional_group == "woody" ~ "shrub",
-                                      functional_group == "solid" ~ "solid_litter",
-                                      TRUE ~ functional_group),
+  mutate(variable_class = case_when(variable_class == "open" ~ "bare_ground",
+                                    variable_class == "woody" ~ "shrub",
+                                    variable_class == "solid" ~ "solid_litter",
+                                      TRUE ~ variable_class),
          course = "Puna",
          plot_id = as.character(plot_id)) %>%
   filter(!is.na(value))
