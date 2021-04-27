f892f8296283bbc36048c66b72815b1f5f37017c
audhalbritter <me@audhalbritter.com>
2021-04-27 08:14:26 +0200

    fix order

--- code/coordinates.R
+++ code/coordinates.R
@@ -6,4 +6,5 @@ library("janitor")
 
 coordinates <- read_excel("data/PU.10_PFTC3.10_2020_Peru_Coordinates.xlsx") %>%
   clean_names() %>%
-  mutate(plot_id = as.character(plot_id))
+  mutate(plot_id = as.character(plot_id)) %>%
+  select(site, treatment, plot_id, burn_year, elevation, latitude, longitude)
