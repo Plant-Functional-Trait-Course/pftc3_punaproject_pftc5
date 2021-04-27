115241fc40d72e8f16518673d99903931e4464e6
audhalbritter <me@audhalbritter.com>
2021-04-27 08:15:29 +0200

    add comment

--- code/coordinates.R
+++ code/coordinates.R
@@ -7,4 +7,4 @@ library("janitor")
 coordinates <- read_excel("data/PU.10_PFTC3.10_2020_Peru_Coordinates.xlsx") %>%
   clean_names() %>%
   mutate(plot_id = as.character(plot_id)) %>%
-  select(site, treatment, plot_id, burn_year, elevation, latitude, longitude)
+  select(site, treatment, plot_id, burn_year, elevation, latitude, longitude, comment)
