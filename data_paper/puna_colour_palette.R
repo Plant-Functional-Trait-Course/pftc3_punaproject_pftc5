b5dce4165b4b93446e6d30441f634ca418e9be97
audhalbritter <me@audhalbritter.com>
2021-05-02 14:31:20 +0200

    add puna colour palette

---
+++ data_paper/puna_colour_palette.R
@@ -0,0 +1,28 @@
+# Plot theme
+
+puna_colour = tribble(~site, ~treatment, ~colour,
+                      "WAY", "C", "#E54D00",
+                      "WAY", "B", "#650505",
+                      "ACJ", "C", "#E58900",
+                      "ACJ", "B", "#7F2929",
+                      "ACJ", "NB", "#420426",
+                      "PIL", "C", "#F4B462",
+                      "PIL", "B", "#C06733",
+                      "PIL", "BB", "#005385", #bonus colour - pick your own
+                      "TRE", "C", "#7AAB76",
+                      "TRE", "NB", "#841457",
+                      "QUE", "C", "#2C9160", #added to get complete elvation gradient
+                      "QUE", "B", "#DAC2A4",
+                      "QUE", "NB", "#D15E7F",
+                      "OCC", "C", "#033311")
+
+puna_site_colour = puna_colour %>%
+  filter(treatment == "C") %>%
+  select(-treatment)
+
+puna_treatment_colour = puna_colour %>%
+  filter(site == "ACJ" |
+         treatment == "BB") %>%
+  select(-site)
+
+
