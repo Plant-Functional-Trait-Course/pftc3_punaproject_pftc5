55223cb560bd314fa03c550b0f4ca696d7e0353e
Erik Kusch <erik@i-solution.de>
2021-05-12 14:18:19 +0200

    Script now automatically installs packages which aren't already present in the user library.
    Subsequent scripts which source this script will not fail upon sourcing due to missing libraries anymore.

--- code/load_libraries.R
+++ code/load_libraries.R
@@ -1,17 +1,39 @@
-# load libraries
-library("here")
-#remotes::install_github("Plant-Functional-Trait-Course/PFTCFunctions")
-library("PFTCFunctions")
-library("osfr")
-library("readxl")
-library("tidyverse")
-library("lubridate")
-#library("tpl")
-library("janitor")
-library("writexl")
-library("vegan")
-library("ggvegan")
-library("broom")
+### LIBRARIES ----
+## CRAN packages
+# custom function that checks whether package is installed and installs a package that isn't before loading it
+install.load.package <- function(x) {
+  if (!require(x, character.only = TRUE))
+    install.packages(x, repos='http://cran.us.r-project.org')
+  require(x, character.only = TRUE)
+}
+# packages available at CRAN and needed here
+package_vec <- c(
+  "here",
+  "remotes",
+  "osfr",
+  "readxl",
+  "tidyverse",
+  "lubridate",
+  #"tpl",
+  "janitor",
+  "writexl",
+  "vegan",
+  "broom"
+)
+# install/load packages as needed
+sapply(package_vec, install.load.package)
 
-# little Helper
+## non-CRAN packages
+if("ggvegan" %in% rownames(installed.packages()) == FALSE){ # check if package is installed
+  remotes::install_github("gavinsimpson/ggvegan")
+}
+library(ggvegan)
+
+if("PFTCFunctions" %in% rownames(installed.packages()) == FALSE){ # check if package is installed
+  remotes::install_github("Plant-Functional-Trait-Course/PFTCFunctions")
+}
+library(PFTCFunctions)
+
+### HELPER FUNCTION ----
 pn <- . %>% print(n = Inf)
+
